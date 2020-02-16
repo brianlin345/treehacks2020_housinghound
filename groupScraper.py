eaad8aea6461586fbdabd99a563a400575f764ce
theTijesuni <tjoyeyemi@gmail.com>
2020-02-15 22:59:16 -0800

    Added the flask initialization code.

--- groupScraper.py
+++ groupScraper.py
@@ -166,3 +166,18 @@ def groupScraperMain():
         print(h)
 
 groupScraperMain()
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('home.html ')
+
+@app.route('/about')
+def index():
+    return render_template('about.html ')
+
+if __name__ == '__main__':
+    app.run(debug = True)
