ddc7b4e5144fd95b32dbd8f62bf3e85283785fe4
Brian Lin <blin1007@berkeley.edu>
2020-02-15 18:31:00 -0800

    Update groupScraper.py

--- groupScraper.py
+++ groupScraper.py
@@ -1,36 +1,16 @@
-import facebook
-import urllib3
 import requests
-
 from requests_html import HTMLSession
-from bs4 import BeautifulSoup
 
 url = 'https://m.facebook.com/groups/ucberkeleyoffcampushousing'
 url2 = 'https://m.facebook.com/groups/1835635240040670/'
 
 
-def checkReaction(text):
-    reactions = ['Likes', 'Like', 'Comments', 'Comment', 'Share', 'Shares']
-    return any([reaction in text for reaction in reactions])
 
-def checkExpand(text):
-    expand_text = "More"
-    return expand_text in text
-
-def checkEnds(text):
-    endText = '·'
-    if text == endText:
-        return True
-    return False
-
-def filterSpan(text):
+def filterPost(text):
     if len(text) <= 0:
         return False
-    if checkReaction(text):
-        return False
-    if checkExpand(text):
-        return False
-    return True
+    else:
+        return True
 
 def findNextPage(r):
     urlID = 'multi_permalinks'
@@ -38,37 +18,51 @@ def findNextPage(r):
         if urlID in url:
             return url
 
-profiles = {}
+def findGroupName(r):
+    group_class = '._de1'
+    group_name = r.html.find(group_class, first = True)
+    return group_name.text
 
-def scrapePage(r):
-    separator = '\\'
-    curr_post = ''
-    posts = []
-    last_token = ''
-    for post in r.html.find('span'):
-        curr_text = post.text
-        if(filterSpan(curr_text)):
-            if checkEnds(curr_text):
-                curr_post = curr_post.replace(last_token, '')
-                posts.append(curr_post)
 
-                curr_name = print(last_token.split(separator))
-                profiles[curr_name] = ''
-
-                curr_post = ''
-            curr_post += curr_text
-            last_token = curr_text
-    return posts
-
-def scraperMain(start_url, pages = 1):
+def scrapePage(r, posts):
     session = HTMLSession()
-    posts = []
+    group_name = findGroupName(r)
+    post_class = '.story_body_container'
+    for post in r.html.find(post_class):
+        curr_poster = post.find('strong')[0].text
+        curr_post = ''
+        for para in post.find('p'):
+            curr_post += para.text
+        if filterPost(curr_post):
+            posts[curr_poster]= curr_post
+
+
+def scraperMain(start_url, pages = 2):
+    session = HTMLSession()
+    posts = {}
+
     for search in range(pages):
-        print('Scraping page ' + start_url)
         req = session.get(start_url)
-        posts.extend(scrapePage(req))
+        print('Scraping page ' + start_url)
+        scrapePage(req, posts)
         start_url = findNextPage(req)
+
+
     return posts
+
+
 
 print(scraperMain(url2))
-print(profiles)
+
+
+def parsePost(post):
+    buyer_words = ['looking for', 'budget', 'searching']
+    seller_words = ['lease', '$', 'resident']
+    post_words = post.split()
+
+
+class Housing:
+    def __init__(self, bedrooms, bathrooms, price):
+        self._bedrooms = bedrooms
+        self._bathrooms = bathrooms
+        self._price = price
